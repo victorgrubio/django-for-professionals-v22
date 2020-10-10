997274b9faa71a2c05a4b517dd7d6b3b90d56da8
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2020-10-10 11:21:12 +0200

    Finished search chapter

--- bookstore/books/urls.py
+++ bookstore/books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, SearchResultsListView
 
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
 
 from .models import Book
 # Create your views here.
@@ -17,3 +18,17 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query | Q(author__icontains=query))
+        )
+    
+    
